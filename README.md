7458c806f773e830ae9a3e34d99c1629cf3b27c0
1-AkM-0 <thepe880@gmail.com>
2025-03-27 00:07:22 -0300

    Add scripts lint:check and lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aplicar no curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home ()
-{
-    return <h1>Lorem lorem lorem lorem ...</h1>
+function Home() {
+  return <h1>Lorem lorem lorem lorem ...</h1>;
 }
 
 export default Home;
